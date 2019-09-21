bfb6ec50cab73da14da18c176772821726e4e255
Jesse Theisen <theiseja@gmail.com>
2019-09-21 13:28:58 -0500

    Setups admin user for django admin page

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/adv/', include('adventure.urls')),
+    path('adventure/', include('adventure.urls')),
 ]
